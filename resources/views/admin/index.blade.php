86d66f639267f76a0ef324778344611638d9755a
Sergey Stepanov <ser1653285@yandex.ru>
2023-08-08 19:07:39 +0300

    fix err

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
 @section('header')
     <header class="page-header">
         <h1 class="page-header__title">Идём<span>в</span>кино</h1>
-        <span class="page-header__subtitle">Администраторррская</span>
+        <span class="page-header__subtitle">Администраторская</span>
     </header>
 @endsection
 
