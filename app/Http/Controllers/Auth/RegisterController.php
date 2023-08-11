3f76ccc2ae86edca606952d3e5c1fe6f9d9e9643
Sergey Stepanov <ser1653285@yandex.ru>
2023-08-11 20:21:31 +0300

    добавил регистрацию

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'string', 'email', 'unique:users', 'max:255' ],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('admin.index');
+    }
+}
--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -13,16 +13,25 @@
                 <h2 class="login__title">Регистрация</h2>
             </header>
             <div class="login__wrapper">
-                <form class="login__form" action={{ route('register') }} method="POST" accept-charset="utf-8">
+                <form class="login__form" action={{ route('register') }} method="POST" accept-charset="utf-8"
+                    autocomplete="off">
                     @csrf
                     <label class="login__label" for="mail">
                         E-mail
                         <input class="login__input" type="email" placeholder="example@domain.xyz" name="email" required>
                     </label>
+                    @error('email')
+                        <p style="font-size: 1.2rem; color: red">{{ $message }}</p>
+                    @enderror
+
                     <label class="login__label" for="pwd">
                         Пароль
                         <input class="login__input" type="password" placeholder="" name="password" required>
                     </label>
+                    @error('password')
+                        <p style="font-size: 1.2rem; color: red">{{ $message }}</p>
+                    @enderror
+
                     <div class="text-center">
                         <input value="Регистрация" type="submit" class="login__button">
                     </div>
