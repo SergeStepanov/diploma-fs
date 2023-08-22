40c47ff54c9f17ef7199eec42040bb4141196b8e
Sergey Stepanov <ser1653285@yandex.ru>
2023-08-22 17:09:27 +0300

    v nabuk

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomePageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home.index');
-})->name('home');
+Route::get('/', [HomePageController::class, 'index'])->name('home');
 
 // Route::get('/test', function () {
 //     return view('admin.register');
