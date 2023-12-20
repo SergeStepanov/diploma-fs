be0181fd9824c4c716be92e264d2dda836286294
Sergey Stepanov <ser1653285@yandex.ru>
2023-12-20 17:08:37 +0300

    fix logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,9 +38,9 @@ class LoginController extends Controller
     {
         Auth::logout();
 
-        $request->session()->invalidate();
+        // $request->session()->invalidate();
 
-        $request->session()->regenerateToken();
+        // $request->session()->regenerateToken();
 
         return redirect()->route('home');
     }
